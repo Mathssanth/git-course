1763aa0e870f63eb688214df3a09e3ef3590b3ad
Matheus Santos <matheus.540@hotmail.com>
2020-09-23 16:49:58 -0300

    Commit de teste hero

--- teste2.cs
+++ teste2.cs
@@ -2,8 +2,24 @@ using System;
 					
 public class Program
 {
+    public class Pessoa{
+        string nome;
+        int idade;
+
+        public void setIdade(int idade){
+            this.idade = idade;
+        }
+        public int getIdade(){
+            return idade;
+        }
+    }
 	public static void Main()
 	{
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.setIdade(21);
+
 		Console.WriteLine("Hello World");
+        Console.WriteLine($"Idade {pessoa1.getIdade}");
 	}
 }
